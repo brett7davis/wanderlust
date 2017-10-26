6533a431c021cc760310bb6dc15e3c766557bee6
Brett <brett7davis@gmail.com>
2017-10-26 18:52:26 +0200

    Changed colors

--- src/App.js
+++ src/App.js
@@ -5,34 +5,50 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <div class="container-fluid black-background white-text">
         <header className="App-header">
-          <img src={"tallinn0.png"} className = "App-logo" alt="logo"/>
+          <img src={"tallinn0.png"} className = "App-logo" alt=""/>
           <h1 className='App-title'>Travels</h1>
         </header>
         <p className="App-intro">
-          Tallinn
-          <img src={"tallinn1.png"} className = "App-picture" alt=""/>
+          Tallinn </p>
+        <p white-text>
+          <img src={"tallinn1.png"} className = "App-picture thin-black-border" alt=""/>
+          I went on a bus trip with Clarice, Pau, Miguel, and Mathilde through the Baltics.  
         </p> 
         <p className="App-intro">
           Helsinki
-          <img src={"finlandia.png"} className = "App-picture" alt=""/>
+          </p>
+          <p>
+          <img src={"finlandia.png"} className = "App-picture thin-black-border" alt=""/>
+          I spent a weekend in Helsinki with Jackie.  The picture is the Sibelius Monument.  
+          We went on a rowboat nearby after taking this picture.
         </p>
         <p className ="App-intro">
         Budapest
-        <img src={"buddy.png"} className = "App-picture" alt=""/>
+        </p>
+          <p>
+        <img src={"buddy.png"} className = "App-picture thin-black-border" alt=""/>
         </p>
         <p className="App-intro">
-          Florence
-          <img src={"flo.png"} className = "App-picture" alt=""/>
+          Florence & Milan
+          </p>
+          <p>
+          <img src={"flo.png"} className = "App-picture thin-black-border" alt=""/>
         </p>
         <p className="App-intro">
-          Bovec
-          <img src={"bov.png"} className = "App-picture" alt=""/>
+          Bovec Valley
+          </p>
+          <p>
+          <img src={"bov.png"} className = "App-picture thin-black-border" alt=""/>
         </p>
         <p className="App-intro">
-          Patagonia
-          <img src={"chilli.png"} className = "App-picture" alt=""/>
+          Chilean Patagonia
+          </p>
+          <p>
+          <img src={"chilli.png"} className = "App-picture thin-black-border" alt=""/>
         </p>
+      </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+
+var pic = 0:
+var picArr = [, ,];
+function transition() {
+	pic + 1 = 3 ? 0: pic + 1;
+	var build = <img border = "0" src = picArray[picCount] +  width = 205  height = 100 >;
+	document.getElementById("imgHolder").innerHTML=build;
+	setTimeout('nextPic()',2000)
+}
